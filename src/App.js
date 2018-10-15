528df28424dd2b97914ac9196caca25e69ad542e
guanine_fedora28_lenovo <1350097782@qq.com>
2018-10-15 11:37:33 +0800

    打包上线路径的问题

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { HashRouter as Router, Route, Switch } from 'react-router-dom';
 
 import ScrollToTop from './components/ScrollToTop';
 import PropsRoute from './components/PropsRoute';
